bb16034968a9a29b402c64186c744a624f047439
delaett <delaett@d6cfbd29-e7e3-0310-a352-f1ecb7ebe6a1>
2004-09-18 11:48:54 +0000

    first working version

--- trunk/src/p2w/mk/p2w.port.subdir.mk
+++ trunk/src/p2w/mk/p2w.port.subdir.mk
@@ -1,38 +1,7 @@
 # Ports 2 www subdir make-file
 
-WMLDIR ?= ${PORTSDIR}/wml
+show-subdirs:
+	@echo ${SUBDIR}
 
-wml:
-	@${_subdir_fragment}
-	@mkdir -p ${WMLDIR}/${PKGPATH}
-	@rm -f ${WMLDIR}/${PKGPATH}/index.html
-	@cd ${.CURDIR} && exec ${MAKE} index.html
-
-WMLTEMPLATES ?= ${P2WDIR}/templates
-.if defined(PORTSTOP)
-WMLTEMPLATE=${WMLTEMPLATES}/wml.top
-.else
-WMLTEMPLATE=${WMLTEMPLATES}/wml.category
-.endif
-
-index.html:
-	@>$@.tmp
-.for d in ${_FULLSUBDIR}
-	@dir=$d; ${_flavor_fragment}; \
-	name=`eval $$toset ${MAKE} _print-wmlfile`; \
-	case $$name in \
-		index) comment='';; \
-		*) comment=`eval $$toset ${MAKE} show=_COMMENT|sed -e 's,^",,' -e 's,"$$,,' |${HTMLIFY}`;; \
-	esac; \
-	cd ${.CURDIR}; \
-	echo "<dt><a href=\"${PKGDEPTH}$$dir/$$name.html\">$d</a><dd>$$comment" >>$@.tmp
-.endfor
-	@cat ${WMLTEMPLATE} | \
-		sed -e 's%%CATEGORY%%'`echo ${.CURDIR} | sed -e 's.*/\([^/]*\)$$\1'`'g' \
-			-e '/%%DESCR%%/r${.CURDIR}/pkg/DESCR' -e '//d' \
-			-e '/%%SUBDIR%%/r$@.tmp' -e '//d' \
-		> ${WMLDIR}/${PKGPATH}/$@
-	@rm $@.tmp
-
-_print-wmlfile:
-	@echo "index"
+pkg-path:
+	@echo ${PKGPATH}
